5b8986716b87eb3ad91aa03105ac1e55a9778a27
vladnik <vladnik@demandbase.com>
2015-10-23 09:24:15 -0700

    Initial Commit

---
+++ ebs-attach-mount.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Parse input arguments
+while [[ $# > 1 ]]
+do
+key="$1"
+
+# Parse arguments
+case $key in
+  -v|--volume-id)
+  VOLUME="$2"
+  shift # past argument
+  ;;
+  -m|--mount-point)
+  MOUNT="$2"
+  shift # past argument
+  ;;
+  *)
+    # unknown option
+  ;;
+esac
+shift
+done
+
+# Detect free block device
+function detect_free_block_device {  
+  for x in {a..z}
+  do
+    DEVICE="/dev/xvd$x"
+    if [ ! -b $DEVICE ]
+    then
+      break
+    fi
+  done
+}
+
+# Attach and mount volume
+function attach_and_mount_volume {
+  # Get instance id
+  INSTANCE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+  # Extract region
+  REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+  echo Instance ID is $INSTANCE
+  
+  if ! aws ec2 attach-volume --volume-id $VOLUME --instance-id $INSTANCE --device $DEVICE --region $REGION
+  then
+    exit 1
+  fi
+  echo Attaching volume as $DEVICE
+
+  # Waiting for volume to be attached
+  while [ ! -b $DEVICE ]
+  do
+    sleep 2
+  done
+
+  # Create directory
+  mkdir -p $MOUNT
+  # Mount volume
+  mount $DEVICE $MOUNT
+  echo Mounted volume as $MOUNT
+}
+
+# Detach volume
+function detach {
+  # Detach volume
+  echo Unmounting and detaching volume
+  umount $MOUNT
+  aws ec2 detach-volume --volume-id $VOLUME --region $REGION
+}
+
+# Run steps
+detect_free_block_device
+attach_and_mount_volume
+trap detach EXIT
+
+# Run infinite loop
+while :
+do
+  sleep 60
+done
